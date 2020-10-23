57a4e766f8646b482047bf94a10812e1d5dd3b19
garyemerson <gmohammadioun@gmail.com>
2020-10-23 12:33:38 -0700

    update

--- shell/profile.sh
+++ shell/profile.sh
@@ -56,13 +56,13 @@ jj() {
     fi
 }
 gssh() {
-    ssh -t $@ "bash --rcfile <(echo $(cat $HOME/.profile | gzip | base64 | tr -d \"\\r\\n\") | base64 --decode | gunzip)"
+    ssh -t "$@" "bash --rcfile <(echo $(cat $HOME/.profile | gzip | base64 | tr -d \"\\r\\n\") | base64 --decode | gunzip)"
 }
 gscript() {
     #tmp=$(mktemp)
     #script -q /dev/null $@ > $tmp
     #cat $tmp
-    script -q /dev/null $@
+    script -q /dev/null "$@"
 }
 du-fmt() {
     du -sh "$@" | sort -hr
@@ -72,9 +72,9 @@ bench-url() {
     curl -s -w 'Testing Website Response Time for %{url_effective}\n\nDNS Lookup Time:\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null $1
 }
 transfer-config() {
-    scp -r ~/{.gitconfig,.tmux.conf,.profile,.vimrc,.vim} $@
+    scp -r ~/{.gitconfig,.tmux.conf,.profile,.vimrc,.vim} "$@"
 }
-foo() {
+ctrl-f() {
     echo -n $PWD
     #echo -n $(printf '%08d' $RANDOM)
     #sleep 1
@@ -99,7 +99,7 @@ if [ -t 1 ]; then
     # Key bindings, up/down arrow searches through history
     #bind '"\e[A": "echo foobar\n"'
     bind -x '"\C-j": "jj"'
-    bind -x '"\C-f": "foo"'
+    bind -x '"\C-f": "ctrl-f"'
     bind '"\e[A": history-search-backward'
     bind '"\e[B": history-search-forward'
     bind '"\eOA": history-search-backward'
